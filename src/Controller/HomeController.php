2340a9abc9cda5f694a24b84ec7f64fce142300a
aggregotech8 <lucas.charrier13@gmail.com>
2025-09-10 01:04:33 +0200

    HTML+css changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(CallApiService $callApiService): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/home.html.twig', [
             'balise' => $callApiService->getAllData()
         ]);
     }
